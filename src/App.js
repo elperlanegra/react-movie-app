b99bedf0c045de6088c3f9dcdcc9d6a94590e035
Manuel Duarte <carlosd99m@gmail.com>
2021-05-10 16:53:19 -0600

    How to save the Search Input to state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async () => {
 		const url = `http://www.omdbapi.com/?s=batman&apikey=61228e1d`;
@@ -23,6 +26,13 @@ const App = () => {
 
 	return (
 		<div className='container-fluid movie-app'>
+            <div className="row d-flex align-items-center mt-4 mb-4">
+
+                <MovieListHeading heading={movies} />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+
+            </div>
+
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
